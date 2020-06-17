681bbafa4654aff43842cfd4f70cd6c053491199
Szanka <szankabal@gmail.com>
2020-06-17 16:28:42 +0200

    fixed condition

--- backend/src/app/controllers/authorization.ts
+++ backend/src/app/controllers/authorization.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from "express";
 import { userRole } from "../models/user"
 
 export const authorization = (req: Request, res: Response, next: NextFunction) => {
-  if(userRole.ADMIN.includes(res.locals.user.role)) {
+  if(res.locals.user.role == userRole.ADMIN) {
     next();
   } else {
     res.sendStatus(403);
