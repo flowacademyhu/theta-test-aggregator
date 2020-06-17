fde47e25f4b6d7f98e006253f5fcc1d39aa1e748
fehercs <feher.csongor@yahoo.com>
2020-06-17 14:02:13 +0200

    method name fix

--- backend/src/lib/querryParamHandlers/filterEndTimeRange.ts
+++ backend/src/lib/querryParamHandlers/filterEndTimeRange.ts
@@ -1,7 +1,7 @@
 import { Request } from 'express';
 import { QueryBuilder } from 'knex';
 
-export const filterSimulationStartTime = (req: Request, query: QueryBuilder) => {
+export const filterSimulationEndTime = (req: Request, query: QueryBuilder) => {
   if (req.query.ended_after && req.query.ended_before) {
     query.whereBetween('start_timestamp', [req.query.ended_after, req.query.ended_before])
   }
