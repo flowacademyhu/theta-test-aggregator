ebd5e381c9482870b9c67f52bb20b8f10844a202
Viktor Mahler <vikitorony@gmail.com>
2020-06-18 10:49:05 +0200

    authGuard not working properly

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,17 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoginComponent } from './components/login/login.component';
-import { AngularMaterialModule } from './angular-material.module';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { LoggedInComponent } from './components/logged-in/logged-in.component';
-import { RouterModule, Routes } from '@angular/router';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {LoginComponent} from './components/login/login.component';
+import {AngularMaterialModule} from './angular-material.module';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {LoggedInComponent} from './components/logged-in/logged-in.component';
+import {RouterModule, Routes} from '@angular/router';
+import {AuthGuard} from './auth.guard';
+import {UserRole} from './models/user.model';
 
 const routes: Routes = [
-  {path: '', component: LoginComponent},
-  {path: 'logged-in', component: LoggedInComponent},
+  {
+    path: '',
+    component: LoginComponent,
+  },
+  {
+    path: 'logged-in',
+    canActivate: [AuthGuard],
+    data: { role: [UserRole.USER] [UserRole.ADMIN] },
+    component: LoggedInComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
 ];
 
 @NgModule({
@@ -28,7 +42,9 @@ const routes: Routes = [
     ReactiveFormsModule,
     RouterModule.forRoot(routes)
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
