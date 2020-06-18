f37ef79a5d4f993e74cad77a9d18e41721b36caa
GallDavid <gall1218@yahoo.com>
2020-06-18 15:38:49 +0200

    partial

--- backend/src/app/models/user.ts
+++ backend/src/app/models/user.ts
@@ -1,12 +1,12 @@
 import { userRole } from "../../lib/enums/enum"
 
 export interface User {
-  id?: string;
-  password_hash?: string;
-  email?: string;
-  git_user?: string;
-  role?: userRole;
-  notification?: boolean;
+  id: string;
+  password_hash: string;
+  email: string;
+  git_user: string;
+  role: userRole;
+  notification: boolean;
   created_at?: string;
   updated_at?: string
 }
