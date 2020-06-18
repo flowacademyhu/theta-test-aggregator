fcad76e3e98c439b293fcacb48a64714364dddad
Viktor Mahler <Vikitorony@users.noreply.github.com>
2020-06-18 11:24:05 +0200

    I23 login page (#62)
    * login page design
    
    * authService added, login works
    
    * login form values fix

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,13 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './components/login/login.component';
+import { AngularMaterialModule } from './angular-material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LoggedInComponent } from './components/logged-in/logged-in.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'logged-in', component: LoggedInComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LoggedInComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
