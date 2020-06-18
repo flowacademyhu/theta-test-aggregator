f7aba7ff7dcb38718ddb6fbb79130906154c6189
Szanka <szankabal@gmail.com>
2020-06-18 16:04:22 +0200

    sendMail added in router

--- backend/src/app/routers/simulationResult.ts
+++ backend/src/app/routers/simulationResult.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import * as simulationResultController from '../controllers/simulationResult';
+import { sendMail } from '../controllers/sendmail';
 
 export const router:Router = Router({ mergeParams: true });
 
 router.get('/', simulationResultController.index);
 router.get('/:id', simulationResultController.show);
 router.post('/', simulationResultController.initialize);
+router.use(sendMail);
 router.put('/:id', simulationResultController.update);
 router.delete('/:id', simulationResultController.destroy);
