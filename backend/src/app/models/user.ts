7c7123977f139eaaf88eb20685fb138cf4a113ca
GallDavid <42264207+GallDavid@users.noreply.github.com>
2020-06-19 08:19:30 +0200

    I66 enum and migra (#75)
    * migration fix
    
    * enum import
    
    * enum import
    
    * userRole import path change
    
    * tablename change
    
    * enum import path change
    
    * tablename and limit, offset change
    
    * enum import, tablename change
    
    * partial
    
    * partial
    
    * create enum.ts
    
    * swagger, router change
    
    * create enums.ts
    
    * enum path
    
    * enum path
    
    * enum path
    
    * enum path
    
    * partial change
    
    * enum path
    
    * enum path

--- backend/src/app/models/user.ts
+++ backend/src/app/models/user.ts
@@ -1,15 +1,12 @@
+import { userRole } from "../../lib/enums"
+
 export interface User {
-  id?: string;
-  password_hash?: string;
-  email?: string;
-  git_user?: string;
-  role?: userRole;
-  notification?: boolean;
+  id: string;
+  password_hash: string;
+  email: string;
+  git_user: string;
+  role: userRole;
+  notification: boolean;
   created_at?: string;
   updated_at?: string
-}
-
-export enum userRole {
-  ADMIN = 'admin',
-  USER = 'user'
 }
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,34 +1,33 @@
 import * as express from 'express';
-import { Request, Response } from 'express';
+import { Application } from 'express';
 import * as createMiddleware from 'swagger-express-middleware';
 import { SwaggerMiddleware } from 'swagger-express-middleware';
-import { database } from './lib/database';
 import { authentication } from './lib/authentication';
+import * as swaggerUi from 'swagger-ui';
+import * as swaggerDocument from '../config/swagger.json';
+import { router } from './app/routers';
 
-const app = express();
-const { PORT = 3000 } = process.env;
+const app: Application = express();
 
 createMiddleware('config/swagger.json', app, (err, middleware: SwaggerMiddleware) => {
   if (err) {
-    console.error(err);
+    console.log(err);
   }
 
-  app.use(
-    middleware.metadata(),
-    middleware.CORS(),
-    middleware.parseRequest(),
-    middleware.validateRequest()
-  );
+  app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-  app.get('/', async (req: Request, res: Response) => {
-    res.json({
-      message: 'hello world',
-    });
-  });
+  app.use(middleware.metadata());
+  app.use(middleware.CORS());
+  app.use(middleware.parseRequest());
+  app.use(middleware.validateRequest());
+
+  app.use(express.json());
+  const { PORT = 3000 } = process.env;
+
+  app.use(authentication);
+  app.use(router);
 
   app.listen(PORT, () => {
     console.log(`server started at http://localhost:${PORT}`);
   });
-
-  app.use(authentication);
 });
