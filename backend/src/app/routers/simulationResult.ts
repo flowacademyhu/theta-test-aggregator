0c0d93c3ae7eba95568767cc8e2f23760a1c27bb
fehercs <feher.csongor@yahoo.com>
2020-06-20 17:51:56 +0200

    added authorization to route

--- backend/src/app/routers/simulationResult.ts
+++ backend/src/app/routers/simulationResult.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import * as simulationResultController from '../controllers/simulationResult';
+import { authorization } from '../controllers/authorization'
 
 export const router:Router = Router({ mergeParams: true });
 
@@ -8,4 +9,5 @@ router.get('/:id', simulationResultController.show);
 router.post('/', simulationResultController.initialize);
 router.put('/:id', simulationResultController.update);
 router.delete('/:id', simulationResultController.destroy);
+router.use(authorization);
 router.put('/:id/invalidate', simulationResultController.invalidate);
