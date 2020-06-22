7a42e91bb73602bb1eb40f74dd6a847f2a0e77cd
GallDavid <gall1218@yahoo.com>
2020-06-22 13:10:07 +0200

    add apikey Router

--- backend/src/app/routers/index.ts
+++ backend/src/app/routers/index.ts
@@ -1,9 +1,11 @@
 import { router as userRouter } from './user';
 import { router as simulationResultRouter } from './simulationResult';
 import { router as loginRouter } from './login';
+import { router as apiKeyRouter } from './apiKey';
 import { Router } from 'express';
 
 export const router: Router = Router({mergeParams: true});
-router.use('/api/user', userRouter);
-router.use('/api/simulationResult', simulationResultRouter);
-router.use('/api/login', loginRouter);
+router.use('/user', userRouter);
+router.use('/simulationResult', simulationResultRouter);
+router.use('/login', loginRouter);
+router.use('/apiKey', apiKeyRouter);
