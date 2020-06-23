e0267ff3fdb6f814f56c028b8bef2522eee6d968
Szanka <szankabal@gmail.com>
2020-06-23 16:37:47 +0200

    bug fix

--- backend/src/app/routers/simulationResult.ts
+++ backend/src/app/routers/simulationResult.ts
@@ -1,12 +1,10 @@
 import { Router } from 'express';
 import * as simulationResultController from '../controllers/simulationResult';
-import { sendMail } from '../controllers/sendmail';
 
 export const router:Router = Router({ mergeParams: true });
 
 router.get('/', simulationResultController.index);
 router.get('/:id', simulationResultController.show);
 router.post('/', simulationResultController.initialize);
-router.put('/:id', sendMail);
 router.put('/:id', simulationResultController.update);
 router.delete('/:id', simulationResultController.destroy);
