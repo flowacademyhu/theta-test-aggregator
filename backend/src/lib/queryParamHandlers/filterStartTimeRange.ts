75dd05b2e30bba8dff37f26630bfc6efca292a74
fehercs <feher.csongor@yahoo.com>
2020-06-23 12:01:34 +0200

    Fixed query handler methods

--- backend/src/lib/queryParamHandlers/filterStartTimeRange.ts
+++ backend/src/lib/queryParamHandlers/filterStartTimeRange.ts
@@ -3,7 +3,7 @@ import { QueryBuilder } from 'knex';
 
 export const filterSimulationStartTime = (req: Request, query: QueryBuilder) => {
   if (req.query.started_after && req.query.started_before) {
-    query.whereBetween('start_timestamp', [req.query.started_after, req.query.started_before])
+    query.whereBetween('start_timestamp', [+req.query.started_after, +req.query.started_before])
   }
   return query;
 };
