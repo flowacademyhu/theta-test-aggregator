acb2fe50bf6d15b16ccf5ae69a27c22423553c9e
GallDavid <gall1218@yahoo.com>
2020-06-24 13:49:46 +0200

    creata Apikey list

--- frontend/src/app/components/api-key-manager/api-key-manager.component.ts
+++ frontend/src/app/components/api-key-manager/api-key-manager.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, DoCheck, OnDestroy } from '@angular/core';
+import { ApiKey } from 'src/app/models/apiKey-model';
+import { Subscription } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { ApikeyService } from '../../services/apikey.service';
 
 @Component({
   selector: 'app-api-key-manager',
   templateUrl: './api-key-manager.component.html',
   styleUrls: ['./api-key-manager.component.css']
 })
-export class ApiKeyManagerComponent implements OnInit {
+export class ApiKeyManagerComponent implements OnInit, DoCheck, OnDestroy {
 
-  constructor() { }
+  constructor(private apiKeyService: ApikeyService, private dialog: MatDialog) { }
+
+  public apikeys: ApiKey[];
+  subscriptions$: Subscription[] = [];
 
   ngOnInit(): void {
+    this.subscriptions$.push(this.apiKeyService.apikeys$.subscribe(apikeys => {
+      this.apikeys = apikeys;
+    }));
   }
 
+  ngDoCheck(): void {
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions$.forEach(sub => sub.unsubscribe());
+  }
 }
