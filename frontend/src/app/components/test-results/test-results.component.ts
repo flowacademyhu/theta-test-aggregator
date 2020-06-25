00c4b7fdbab1bf7cfc4bdddea5b490afbd383378
Adanosz <reti925@gmail.com>
2020-06-25 15:30:05 +0200

    I30 all simulations page update

--- frontend/src/app/components/test-results/test-results.component.ts
+++ frontend/src/app/components/test-results/test-results.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit} from '@angular/core';
 import { TestService } from '../../services/test.service';
 import { Test } from 'src/app/models/test.model';
 import { Subscription } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+
 
 
 @Component({
@@ -11,8 +14,10 @@ import { Subscription } from 'rxjs';
 })
 export class TestResultsComponent implements OnInit {
 
-  constructor(private testService: TestService) {
+  constructor(private testService: TestService, private route: ActivatedRoute, private authService: AuthService) {
   }
+
+  public user;
 
   public tests: Test[];
   subscriptions$: Subscription[] = [];
@@ -22,7 +27,10 @@ export class TestResultsComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.testService.fetchTests();
+    this.route.data.subscribe((data) => {
+      this.tests=data.tests
+    }),
+    this.authService.getCurrentUser().subscribe((data) => {this.user = data});
     this.subscriptions$.push(this.testService.tests$.subscribe(tests => {
       this.tests = tests;
     }));
