bb9a5f000716fa312be9a4781280102093bd758e
SutyaM <61408896+SutyaM@users.noreply.github.com>
2020-06-25 10:03:32 +0200

    I82 frontend connected to backend -user management (#102)
    * frontend connected to backend -user management
    
    * boolean values fixed for update/add user, password is no longer required to be updated every time on user update
    
    * PR comment issues fixed
    
    * fetchUsers() removed from update-user.component.ts
    
    * return value added to addUser and updateUser
    
    * error is caught in login component instead of authService
    
    * type given to errors
    
    * changed loggedInUser$ to private

--- frontend/src/app/components/add-user/add-user.component.ts
+++ frontend/src/app/components/add-user/add-user.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
 import { UserRole } from 'src/app/models/user.model';
-import { v4 as uuid } from 'uuid';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { catchError } from 'rxjs/operators';
+import { HttpErrorResponse } from '@angular/common/http';
+import { throwError } from 'rxjs';
 
 @Component({
   selector: 'app-add-user',
@@ -13,12 +13,12 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class AddUserComponent implements OnInit {
 
-  constructor(@Inject(MAT_DIALOG_DATA) private data: any,  private userService: UserService, private router: Router) { }
+  constructor(private userService: UserService) { }
 
   roles: UserRole[] = [UserRole.ADMIN, UserRole.USER];
+  public errors: string[] = [];
 
   public addForm: FormGroup  = new FormGroup({
-    id: new FormControl(uuid(), [Validators.required]),
     password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
     email: new FormControl(null, [Validators.required, Validators.email]),
     git_user: new FormControl(null, [Validators.required, Validators.pattern(/^\S*$/)]),
@@ -27,8 +27,10 @@ export class AddUserComponent implements OnInit {
   });
 
   public onAdd() {
-    console.log(this.addForm.value)
-    this.userService.addUser(this.addForm.value);
+    this.userService.addUser(this.addForm.value).pipe( catchError((error: HttpErrorResponse) => {
+      return throwError(error.error.message);
+    })).subscribe((data) => {}, (error) => {this.errors = error});
+    this.userService.fetchUsers();
   }
 
   ngOnInit(): void {
