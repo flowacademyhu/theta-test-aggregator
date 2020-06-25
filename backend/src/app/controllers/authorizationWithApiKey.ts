4da72df09a0fbe24ec1ab6ce49120cad7cddfdb9
fehercs <feher.csongor@yahoo.com>
2020-06-25 15:45:41 +0200

    added method for checking expired keys

--- backend/src/app/controllers/authorizationWithApiKey.ts
+++ backend/src/app/controllers/authorizationWithApiKey.ts
@@ -2,11 +2,18 @@ import { Request, Response, NextFunction } from "express";
 import { ApiKey } from "../models/apiKey";
 import { database } from "../../lib/database";
 import { tableName } from '../../lib/tableName';
+import * as moment from 'moment';
+
+const notExpired = (apiKey: ApiKey): boolean => {
+  console.log(apiKey.expires_at + " " + moment());
+  console.log(moment().isBefore(apiKey.expires_at, 'second'));
+  return moment().isBefore(apiKey.expires_at, 'second');
+}
 
 export const authorization = async (req: Request, res: Response, next: NextFunction) => {
   if(req.headers.apikey) {
     const apiKey: ApiKey = await database(tableName.API_KEYS).select().where({ key: req.headers.apikey }).first();
-    if (apiKey && apiKey.key === req.headers.apikey) {
+    if (apiKey && apiKey.key === req.headers.apikey && notExpired(apiKey)) {
       return next();
     }
     else {
