77bcbf8401d72c69caf9af41074f9d90d4bbd04c
GallDavid <gall1218@yahoo.com>
2020-06-26 11:02:07 +0200

    create new router

--- backend/src/app/routers/index.ts
+++ backend/src/app/routers/index.ts
@@ -2,6 +2,7 @@ import { router as userRouter } from './user';
 import { router as simulationResultRouter } from './simulationResult';
 import { router as loginRouter } from './login';
 import { router as apiKeyRouter } from './apiKey';
+import { router as userProfileRouter } from './userProfile';
 import { Router } from 'express';
 import { router as mailerRouter } from './mailer';
 
@@ -11,3 +12,4 @@ router.use('/api/simulationResult', mailerRouter);
 router.use('/api/simulationResult', simulationResultRouter);
 router.use('/api/login', loginRouter);
 router.use('/api/apiKey', apiKeyRouter);
+router.use('/api/userProfile', userProfileRouter);
