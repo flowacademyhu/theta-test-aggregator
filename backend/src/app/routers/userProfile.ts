ee4103b4d22263eb1b20d7a1dbf0fde657e0c902
GallDavid <gall1218@yahoo.com>
2020-06-26 11:53:24 +0200

    fix

--- backend/src/app/routers/userProfile.ts
+++ backend/src/app/routers/userProfile.ts
@@ -3,6 +3,5 @@ import * as userProfileController from '../controllers/userProfile'
 
 export const router: Router = Router({ mergeParams: true });
 
-router.use(userProfileController.userUpdateAuthorization)
 router.get('/', userProfileController.show);
 router.put('/', userProfileController.update);
