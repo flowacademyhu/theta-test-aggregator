77f8a9da0631800444f3b9b1ca93ce8baa04b9fb
GallDavid <42264207+GallDavid@users.noreply.github.com>
2020-06-26 13:10:56 +0200

    I118 user update (#131)
    * create userUpdate router
    
    * create userUpdate controller
    
    * new controller
    
    * creata userProfile Controller
    
    * create new router
    
    * create new router
    
    * create new router
    
    * change
    
    * auth add
    
    * add show
    
    * path
    
    * show add
    
    * swagger for UserProfile
    
    * path
    
    * swagger ifx
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix update

--- backend/src/app/routers/index.ts
+++ backend/src/app/routers/index.ts
@@ -2,11 +2,13 @@ import { router as userRouter } from './user';
 import { router as simulationResultRouter } from './simulationResult';
 import { router as loginRouter } from './login';
 import { router as apiKeyRouter } from './apiKey';
+import { router as userProfileRouter } from './userProfile';
 import { router as authWithApiKeyRouter } from './authorizationWithApiKey';
 import { Router } from 'express';
 import { router as mailerRouter } from './mailer';
 
 export const router: Router = Router({mergeParams: true});
+router.use('/api/user/profile', userProfileRouter);
 router.use('/api/user', userRouter);
 router.use('/api/simulationResult', mailerRouter);
 router.use('/api/simulationResult', authWithApiKeyRouter, simulationResultRouter);
