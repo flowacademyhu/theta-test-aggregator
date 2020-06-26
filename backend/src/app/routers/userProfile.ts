57a49561347c8f9846af8d13312e61c4083b3d7e
GallDavid <gall1218@yahoo.com>
2020-06-26 11:02:27 +0200

    change

--- backend/src/app/routers/userProfile.ts
+++ backend/src/app/routers/userProfile.ts
@@ -1,7 +1,6 @@
 import { Router } from "express";
-import * as userController from '../controllers/user';
 import * as userProfileController from '../controllers/userProfile'
 
 export const router: Router = Router({ mergeParams: true });
 
-router.put('/:id', userProfileController.userUpdateAuthorization, userController.update);
+router.put('/:id', userProfileController.userUpdateAuthorization, userProfileController.update);
