546ee7ec3ef5d50c09cb082b1db386d890b6ffbb
SutyaM <sutya.miklos.cv@gmail.com>
2020-06-29 16:06:11 +0200

    migrations fixed, foreign key added

--- backend/migrations/20200629130613_create-statistics.js
+++ backend/migrations/20200629130613_create-statistics.js
@@ -1,12 +1,16 @@
 exports.up = (knex) => {
   return knex.schema.createTable('statistics', (table) => {
-    table.increments('id');
-    table.bigInteger('start_timestamp').unsinged();
+    table.increments('id').primary();
+    table.string('simulation_result_id', [50]);
+    table.bigInteger('start_timestamp').unsigned();
     table.string('method');
     table.string('endpoint');
-    table.float('measurement');
+    table.float('measurement').unsigned();
+    table.boolean('invalid').defaultTo(false);
     table.timestamp('updated_at').defaultTo(knex.raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
     table.timestamp('created_at').defaultTo(knex.raw('CURRENT_TIMESTAMP'));
+
+    table.foreign('simulation_result_id').references('id').inTable('simulation_results');
   });
 };
 
