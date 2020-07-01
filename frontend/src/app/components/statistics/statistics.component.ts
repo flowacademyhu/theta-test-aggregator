b279f128192a68adff8bb0863f7d1130246a840e
SutyaM <sutya.miklos.cv@gmail.com>
2020-07-01 10:53:18 +0200

    google chart changed to ng2-chart

--- frontend/src/app/components/statistics/statistics.component.ts
+++ frontend/src/app/components/statistics/statistics.component.ts
@@ -18,64 +18,41 @@ export class StatisticsComponent implements OnInit {
     responsive: true,
   };
 
-  public barChartLabels: Label[];
+  public barChartLabels: Label[] = [];
   public barChartType: ChartType = 'bar';
   public barChartLegend = true;
   public barChartPlugins = [];
+  public barChartData: ChartDataSets[] = [
+    {data: [], label: 'Runtime in nanosec'}
+  ]
 
   public statistics: Statistic[];
 
-  public myData = [];
-  public chartColumns: String[] = ['Timestamp', 'Measurements (ms)'];
-  public height: number = 400;
-  public width: number = 550;
   public method: string;
   public endpoint: string;
-  public stats = [];
-
-  public logStatistics() {
-    console.log(this.statistics);
-    console.log(this.myData);
-  }
 
   public showStatistics() {
-    console.log(this.endpoint, this.method);
+    this.barChartLabels = [];
+    this.barChartData[0].data = [];
     this.statistics = this.statisticsService.fetchStatistics(this.endpoint, this.method)
     .sort((a, b) => a.timeStamp < b.timeStamp ? -1 : a.timeStamp > b.timeStamp ? 1 : 0);
-    this.myData = [];
     for(let i = 0; i < this.statistics.length; i++) {
-      this.myData.push([new Date(this.statistics[i].timeStamp/1000000).toLocaleString(), this.statistics[i].measurement]);
+      this.barChartLabels.push(new Date(this.statistics[i].timeStamp/1000000).toLocaleString());
+      console.log(this.barChartLabels);
+      this.barChartData[0].data.push(this.statistics[i].measurement);
+      console.log(this.barChartData[0].data)
     }
-    console.log(this.myData);
   }
 
   ngOnInit(): void {
     this.statistics = this.statisticsService.fetchStatistics("api/transaction/brpld145t7lvfjg4oomg/refund", "PUT")
     .sort((a, b) => a.timeStamp < b.timeStamp ? -1 : a.timeStamp > b.timeStamp ? 1 : 0);
     for(let i = 0; i < this.statistics.length; i++) {
-      this.myData.push([new Date(this.statistics[i].timeStamp/1000000).toLocaleString(), this.statistics[i].measurement]);
+      this.barChartLabels.push(new Date(this.statistics[i].timeStamp/1000000).toLocaleString());
+      console.log(this.barChartLabels);
+      this.barChartData[0].data.push(this.statistics[i].measurement);
+      console.log(this.barChartData[0].data)
     }
-
-    for (let i = 0; i < Object.values(this.statisticsService.simulation).length; i++) {
-      for (let j = 0; j < Object.values(this.statisticsService.simulation)[i].length; j++) {
-        if (Object.values(this.statisticsService.simulation)[i][j].measurement !== '') {
-          const string = Object.values(this.statisticsService.simulation)[i][j].endpoint.split(" ");
-          const method = string[0];
-          const string2 = string[1].split("/api")
-          const endpoint = "/api" + string2[1];
-          const measurement = parseFloat(Object.values(this.statisticsService.simulation)[i][j].measurement.slice(0, -2));
-          const stat = {
-            method: method,
-            endpoint: endpoint,
-            measurement: measurement
-          }
-          this.stats.push(stat);
-        }
-      }
-    }
-    //console.log(Object.values(this.statisticsService.simulation)[0][0].endpoint)
-    console.log(this.stats);
-    console.log(Object.values(this.statisticsService.simulation)[0].length)
   }
 
 }
