41ad3fbbc4f5b87348c8d8561f7e0ec39dc2acd0
Szanka <szankabal@gmail.com>
2020-07-01 10:47:32 +0200

    removed useless spaces

--- frontend/src/app/components/filters/filters.component.ts
+++ frontend/src/app/components/filters/filters.component.ts
@@ -15,21 +15,18 @@ import { FilterParamsModel } from "../../models/filter-params-model";
         <mat-form-field>
           <input matInput formControlName="commit_hash" placeholder="Commit hash">
         </mat-form-field>
-
         <mat-form-field>
           <mat-label>Starting date</mat-label>
             <input matInput [matDatepicker]="picker1" formControlName="started_after">
           <mat-datepicker-toggle matSuffix [for]="picker1"></mat-datepicker-toggle>
           <mat-datepicker #picker1></mat-datepicker>
         </mat-form-field>
-
         <mat-form-field>
           <mat-label>Ending date</mat-label>
             <input matInput [matDatepicker]="picker2" formControlName="started_before">
           <mat-datepicker-toggle matSuffix [for]="picker2"></mat-datepicker-toggle>
           <mat-datepicker #picker2></mat-datepicker>
         </mat-form-field>
-
       </div>
       <button mat-raised-button type="submit" [disabled]="filterForm.pristine">Search</button>
       <button mat-raised-button color="warn" type="button" (click)="onReset()">Reset</button>
