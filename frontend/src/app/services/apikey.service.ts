39c1a5da22444e1113bf98aede9e02549fd3f413
GallDavid <gall1218@yahoo.com>
2020-07-02 15:45:24 +0200

    fixing

--- frontend/src/app/services/apikey.service.ts
+++ frontend/src/app/services/apikey.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { ApiKey } from '../models/apiKey-model';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { tap } from 'rxjs/operators';
 
 @Injectable({
@@ -20,16 +20,7 @@ export class ApikeyService {
     return this.http.get<ApiKey>(environment.baseUrl + `apiKey/${id}`);
   }
 
-  public addApiKey(infinite: string): Observable<string> {
-    return this.http.post<string>(environment.baseUrl + 'apiKey', {
-      params: {
-        infinite: `${infinite}`
-      }
-    });
-  }
-
   public updateApiKey(apikey: ApiKey): Observable<string> {
-    // const httpOptions = { headers: new HttpHeaders({'Content-Type': 'application/json'}), responseType: 'text' as 'json'};
     const httpOptions: object = {responseType: 'text'};
     return this.http.put<string>(environment.baseUrl + `apiKey/${apikey.id}`, apikey, httpOptions);
   }
