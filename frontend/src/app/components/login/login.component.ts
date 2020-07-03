1303d807b23ba59a34c5269d7fa026f4cdc0701c
Adanosz <61408925+Adanosz@users.noreply.github.com>
2020-07-03 13:59:30 +0200

    I24 google authentication #24 (#170)
    * google authentication first steps
    
    * google auth first steps
    
    * google auth added
    
    * Google auth with backend + user avatar from google account
    
    * google auth done
    
    * Google provider url replaced to environment
    
    * refactoring the login.

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -3,6 +3,9 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { HttpErrorResponse } from '@angular/common/http';
+import { SocialAuthService } from "angularx-social-login";
+import { GoogleLoginProvider } from "angularx-social-login";
+import { SocialUser } from "angularx-social-login";
 
 @Component({
   selector: 'app-login',
@@ -10,8 +13,14 @@ import { HttpErrorResponse } from '@angular/common/http';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  
+  user: SocialUser;
+  loggedIn: boolean;
+  email: any;
+  password: any;
+  errorMessage: any;
 
-  constructor(private authService: AuthService, private router: Router) {
+  constructor(private authService: AuthService, private router: Router, private socialAuthService: SocialAuthService) {
   }
 
   public errors: Object;
@@ -24,19 +33,38 @@ export class LoginComponent implements OnInit {
 
   public login() {
     this.authService
-      .login(this.loginForm.value.email, this.loginForm.value.password, this.isChecked)
-      .subscribe(() => {
-        this.router.navigate(['index']);
-      }, (error: HttpErrorResponse) => {
-        this.errors = error;
-        console.log(this.errors);
-      });
+    .login(this.loginForm.value.email, this.loginForm.value.password, this.isChecked)
+    .subscribe(() => {
+      this.router.navigate(['index']);
+    }, (error: HttpErrorResponse) => {
+      this.errors = error;
+      console.log(this.errors);
+    });
   }
 
   rememberMe() {
     this.isChecked = !this.isChecked;
   }
 
+  async signInWithGoogle(): Promise<void> {
+    const googleData = await this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID);
+    const body = {
+      email: googleData.email,
+      password: googleData.idToken
+    };
+    this.loggedIn = (this.user != null);
+    this.authService.loginWithGoogle(this.user.idToken, this.isChecked).subscribe(() => {
+      sessionStorage.setItem('pic', this.user.photoUrl)
+      this.router.navigate(['index'])
+    }, (error: HttpErrorResponse) => {
+      this.errors = error;
+      console.log(this.errors);
+    });
+  }
+
   ngOnInit(): void {
+    this.socialAuthService.authState.subscribe((user) => {
+      this.user = user;
+    });
   }
 }
