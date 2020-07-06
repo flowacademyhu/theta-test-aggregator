af16703c14bbf9c3b910dff759ae565458991f6c
Viktor Mahler <Vikitorony@users.noreply.github.com>
2020-07-06 10:22:25 +0200

    I179 filters.component.ts to .ts .html .css (#181)
    * ts -> ts, html, css
    
    * app.module.ts missing semicolon, extra whitespace fix, installed new dependencies
    
    * quick fix: missing div and form close tag

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -28,7 +28,7 @@ import { FiltersComponent } from './components/filters/filters.component';
 import { TestDetailsComponent } from './components/test-details/test-details.component';
 import { TestResolver } from './resolvers/test.resolver';
 import { SocialLoginModule, SocialAuthServiceConfig } from 'angularx-social-login';
-import { GoogleLoginProvider } from 'angularx-social-login'
+import { GoogleLoginProvider } from 'angularx-social-login';
 import { environment } from 'src/environments/environment';
 import { PayloadTextPrettifyPipe } from './pipes/payload-text-prettify.pipe';
 
@@ -84,14 +84,14 @@ const routes: Routes = [
     },
     {
       provide: 'SocialAuthServiceConfig',
-      useValue: 
+      useValue:
       {
         autoLogin: false,
-        providers: 
+        providers:
         [
           {
             id: GoogleLoginProvider.PROVIDER_ID,
-            provider: new GoogleLoginProvider (environment.GoogleLoginProvider),
+            provider: new GoogleLoginProvider (environment.GoogleLoginProvider)
           },
         ],
       } as SocialAuthServiceConfig,
