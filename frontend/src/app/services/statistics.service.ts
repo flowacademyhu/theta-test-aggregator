e5962f4e2f8a42357e45a9b93b740ea0cde75d0d
SutyaM <sutya.miklos.cv@gmail.com>
2020-07-06 14:41:39 +0200

    put margin on form-field so new header doesn't cover it, fetchStatistic methods have return type

--- frontend/src/app/services/statistics.service.ts
+++ frontend/src/app/services/statistics.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Statistic } from '../models/statistic.model';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +11,11 @@ export class StatisticsService {
 
   constructor(private http: HttpClient) { }
 
-  public fetchStatisticsByEndPointAndMethod(endpoint: string, method: string) {
+  public fetchStatisticsByEndPointAndMethod(endpoint: string, method: string): Observable<Statistic[]> {
     return this.http.get<Statistic[]>(environment.baseUrl + `statistic?endpoint=${endpoint}&method=${method}`);
   };
 
-  public fetchStatistics() {
+  public fetchStatistics(): Observable<Statistic[]> {
     return this.http.get<Statistic[]>(environment.baseUrl + 'statistic');
   }
 
