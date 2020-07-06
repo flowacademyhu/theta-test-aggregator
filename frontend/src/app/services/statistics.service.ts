cc602bba8bfe6a7d6651999fe28d0a056dc5b5d3
SutyaM <sutya.miklos.cv@gmail.com>
2020-07-06 15:16:10 +0200

    statistics service created

---
+++ frontend/src/app/services/statistics.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Statistic } from '../models/statistic.model';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StatisticsService {
+
+  constructor(private http: HttpClient) { }
+
+  public fetchStatisticsByEndPointAndMethod(endpoint: string, method: string): Observable<Statistic[]> {
+    return this.http.get<Statistic[]>(environment.baseUrl + `statistic?endpoint=${endpoint}&method=${method}`);
+  };
+
+  public fetchStatistics(): Observable<Statistic[]> {
+    return this.http.get<Statistic[]>(environment.baseUrl + 'statistic');
+  };
+
+}
