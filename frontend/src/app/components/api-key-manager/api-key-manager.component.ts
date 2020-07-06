73c0e5341b065c3d06590f5184f22143d7061ddf
GallDavid <42264207+GallDavid@users.noreply.github.com>
2020-07-06 10:26:56 +0200

    I36 crud for apikeys without addApikey (#168)
    * add apikey resolver to path
    
    * create apikey service
    
    * create apikey resolver
    
    * click create
    
    * create click
    
    * delete empyt line
    
    * add apikey
    
    * add apikey
    
    * create add-apikey-modal
    
    * update apikey service
    
    * .
    
    * update
    
    * update
    
    * update fix
    
    * update fix
    
    * fix
    
    * fixing
    
    * fixing
    
    * fixing
    
    * fix

--- frontend/src/app/components/api-key-manager/api-key-manager.component.ts
+++ frontend/src/app/components/api-key-manager/api-key-manager.component.ts
@@ -3,6 +3,9 @@ import { ApiKey } from 'src/app/models/apiKey-model';
 import { Subscription } from 'rxjs';
 import { MatDialog } from '@angular/material/dialog';
 import { ApikeyService } from '../../services/apikey.service';
+import { ActivatedRoute } from '@angular/router';
+import { ConfirmDeleteModalComponent } from 'src/app/modals/confirm-delete-modal/confirm-delete-modal.component';
+import { AddApikeyModalComponent } from 'src/app/modals/add-apikey-modal/add-apikey-modal.component';
 
 @Component({
   selector: 'app-api-key-manager',
@@ -11,21 +14,50 @@ import { ApikeyService } from '../../services/apikey.service';
 })
 export class ApiKeyManagerComponent implements OnInit, DoCheck, OnDestroy {
 
-  constructor(private apiKeyService: ApikeyService, private dialog: MatDialog) { }
+  constructor(
+    private apiKeyService: ApikeyService,
+    private dialog: MatDialog,
+    private route: ActivatedRoute
+    ) {
+    }
 
   public apikeys: ApiKey[];
+  public apikey: ApiKey;
   subscriptions$: Subscription[] = [];
 
+ public getApiKeys() {this.apiKeyService.fetchApiKeys().subscribe((data) => {
+    this.apikeys = data;
+  });
+}
+
+  public toggleDeleteModal(apikey) {
+    const dialogRef = this.dialog.open(ConfirmDeleteModalComponent, {
+      data: { git_user: 'this apikey' }
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.apiKeyService.deleteApiKey(apikey.id).subscribe(() => {
+          this.getApiKeys();
+          });
+        }
+      });
+    }
+
+  public extendExpirationDay(apikey) {
+    this.apiKeyService.updateApiKey(apikey).subscribe(() => {
+      this.getApiKeys();
+      });
+    }
+
   ngOnInit(): void {
-    this.subscriptions$.push(this.apiKeyService.apikeys$.subscribe(apikeys => {
-      this.apikeys = apikeys;
-    }));
+   this.route.data.subscribe((data) => {
+     this.apikeys = data.apikeys;
+    });
   }
 
   ngDoCheck(): void {
   }
 
   ngOnDestroy(): void {
-    this.subscriptions$.forEach(sub => sub.unsubscribe());
   }
 }
