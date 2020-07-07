1948cd80cfe1f9245d11e723a7f49cc1fbeb48ca
SutyaM <sutya.miklos.cv@gmail.com>
2020-07-07 16:32:36 +0200

    serializer created, destroy and create controllers created

--- backend/src/app/controllers/customFilter.ts
+++ backend/src/app/controllers/customFilter.ts
@@ -1,4 +1,5 @@
 import { CustomFilter } from '../models/customFilter';
+import * as customFilterSerializer from '../serializers/customFilter';
 import { database } from "../../lib/database";
 import { Request, Response } from "express";
 import { QueryBuilder } from 'knex';
@@ -12,9 +13,49 @@ export const index = async (req: Request, res: Response) => {
     limitQuery(req, query);
     offsetQuery(req, query);
     const customFilters: Array<CustomFilter> = await query;
-    res.json(customFilters);
+    res.json(customFilterSerializer.index(customFilters));
   } catch (error) {
     console.log(error);
     res.sendStatus(500);
   }
+};
+
+const convertUnix = (date: Date) : number => {
+  return (new Date(date).getTime())*1000000;
 }
+
+export const create = async (req: Request, res: Response) => {
+  const started_after = convertUnix(req.body.started_after);
+  const started_before = convertUnix(req.body.started_before);
+  try {
+    const customFilter: CustomFilter = {
+      name: req.body.name,
+      user_id: res.locals.user.id,
+      triggered_by: req.body.triggered_by,
+      commit_hash: req.body.commit_hash,
+      status: req.body.status,
+      started_after: started_after,
+      started_before: started_before
+    }
+    await database(tableName.CUSTOM_FILTERS).insert(customFilter);
+    res.sendStatus(201);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+};
+
+export const destroy = async (req: Request, res: Response) => {
+  try {
+    const customFilter = await database(tableName.CUSTOM_FILTERS).select().where({ id: req.params.id }).first();
+    if (customFilter) {
+      await database(tableName.CUSTOM_FILTERS).delete().where({ id: req.params.id });
+      res.sendStatus(204);
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+};
--- backend/src/app/models/customFilter.ts
+++ backend/src/app/models/customFilter.ts
@@ -1,14 +1,14 @@
 import { SimulationResultStatus } from "../../lib/enums";
 
 export interface CustomFilter {
-  id: number;
+  id?: number;
   name: string;
   user_id: string;
   triggered_by?: string;
   commit_hash?: string;
   status?: SimulationResultStatus;
-  started_after?: Date;
-  started_before?: Date;
+  started_after?: number;
+  started_before?: number;
   created_at?: string;
   updated_at?: string;
 }
