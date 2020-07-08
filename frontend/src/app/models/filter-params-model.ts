02e4f30a83135e3ddb2f73f0034564b2643fc509
fehercs <51792360+fehercs@users.noreply.github.com>
2020-07-08 09:04:40 +0200

    I195 server side pagination (#199)
    * edited test result component
    
    * edited filter model and created testResponse model
    
    * edited test service
    
    * created common fetch method
    
    * typo fix
    
    * fix
    
    * paginator filter fix

--- frontend/src/app/models/filter-params-model.ts
+++ frontend/src/app/models/filter-params-model.ts
@@ -2,6 +2,8 @@ export interface FilterParamsModel {
   triggered_by?: string;
   commit_hash?: string;
   started_after?: number;
-  started_before?: number; 
+  started_before?: number;
   status?: 'SUCCESS' | 'FAILED' | 'ERROR' | 'UNKNOWN';
+  limit?: number;
+  offset?: number;
 }
