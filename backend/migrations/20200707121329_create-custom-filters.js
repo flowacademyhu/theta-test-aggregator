9a8f6f7ca6505efca6d3072960f9e8802605d702
SutyaM <sutya.miklos.cv@gmail.com>
2020-07-08 10:57:50 +0200

    routes and swagger done for customfilters

--- backend/migrations/20200707121329_create-custom-filters.js
+++ backend/migrations/20200707121329_create-custom-filters.js
@@ -1,16 +1,15 @@
 exports.up = (knex) => {
   return knex.schema.createTable('custom_filters', (table) => {
     table.increments('id').primary();
-    table.string('name').unique();
+    table.string('name');
     table.string('user_id');
     table.string('triggered_by');
     table.string('commit_hash');
+    table.string('status');
     table.bigInteger('started_after').unsigned();
     table.bigInteger('started_before').unsigned();
     table.timestamp('created_at').defaultTo(knex.raw('CURRENT_TIMESTAMP'));
     table.timestamp('updated_at').defaultTo(knex.raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
-
-    table.foreign('user_id').onDelete('CASACDE').references('id').inTable('users');
   });
 };
 
